528add3f8e1134f1365874ea8c3c6a9c9e24c48d
aakash10802 <aakashpc123@gmail.com>
2025-01-27 16:29:55 +0530

    Add Book model, serializers, and API views for listing and managing books

--- PROJECT/PROJECT/urls.py
+++ PROJECT/PROJECT/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# PROJECT/urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('APP.urls')),
-    path('api/ap', include('api_test.urls')),
-    
+    path('api/ap/', include('api_test.urls')),   
 ]
 
